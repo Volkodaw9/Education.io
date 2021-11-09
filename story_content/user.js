fc879740a2e4a713621840ea9e3721cb195b9481
Volkodaw9 <94014163+Volkodaw9@users.noreply.github.com>
2021-11-09 22:55:13 +0300

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,639 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5q6CnaTV7Tv":
+        Script1();
+        break;
+      case "6WDecMpRXcJ":
+        Script2();
+        break;
+      case "6Flk6tgtGft":
+        Script3();
+        break;
+      case "5xrHRjPTiiS":
+        Script4();
+        break;
+      case "5ckYNUHTfSM":
+        Script5();
+        break;
+      case "6Egw7VjoljH":
+        Script6();
+        break;
+      case "5pieCTdcGis":
+        Script7();
+        break;
+      case "6ZxOWogwi5j":
+        Script8();
+        break;
+      case "6JTNDsJJGOO":
+        Script9();
+        break;
+      case "6L2iQnBfRUO":
+        Script10();
+        break;
+      case "5h0qexMsp9Y":
+        Script11();
+        break;
+      case "67DHAaBJPc0":
+        Script12();
+        break;
+      case "5zgZbWyy0Ax":
+        Script13();
+        break;
+      case "5aEzI1LgCH6":
+        Script14();
+        break;
+      case "65dK0WA7ki7":
+        Script15();
+        break;
+      case "6UJUGVLFQTC":
+        Script16();
+        break;
+      case "6TU6Z6iKbnv":
+        Script17();
+        break;
+      case "6IqK1Q1boKd":
+        Script18();
+        break;
+      case "5nlP6OcAw7m":
+        Script19();
+        break;
+      case "687PWpv2sb4":
+        Script20();
+        break;
+      case "5qHkwp9JxN4":
+        Script21();
+        break;
+      case "6CDWSAfcgC7":
+        Script22();
+        break;
+      case "6O5A8AIhJl9":
+        Script23();
+        break;
+      case "5txriGKXgeT":
+        Script24();
+        break;
+      case "6qgOkWOIFCZ":
+        Script25();
+        break;
+      case "5olY41MsDoq":
+        Script26();
+        break;
+      case "68nJzkWh5xw":
+        Script27();
+        break;
+      case "60atnZwD0zN":
+        Script28();
+        break;
+      case "6PAmIHqB8Ar":
+        Script29();
+        break;
+      case "6QA5Ygirm7N":
+        Script30();
+        break;
+      case "5fnOZTZlTNR":
+        Script31();
+        break;
+      case "5sLSBUl2NTl":
+        Script32();
+        break;
+      case "6f0CdrLXWd1":
+        Script33();
+        break;
+      case "6EwYckOo0zA":
+        Script34();
+        break;
+      case "6jc5at444XW":
+        Script35();
+        break;
+      case "6FjIxFsmbKA":
+        Script36();
+        break;
+      case "6hK12Iafmry":
+        Script37();
+        break;
+      case "5ap30npL3hE":
+        Script38();
+        break;
+      case "6e9gLZ6tcD2":
+        Script39();
+        break;
+      case "5cLj6VKuvOu":
+        Script40();
+        break;
+      case "62syoonD4wR":
+        Script41();
+        break;
+      case "62nyZh8oSzJ":
+        Script42();
+        break;
+      case "5zsfRwDi5IN":
+        Script43();
+        break;
+      case "6cqW3w8O7UY":
+        Script44();
+        break;
+      case "5YVVkqJOKiY":
+        Script45();
+        break;
+      case "5e5WzqVcJ6w":
+        Script46();
+        break;
+      case "61LONt8Q9s8":
+        Script47();
+        break;
+      case "6PvNdRBkVq5":
+        Script48();
+        break;
+      case "5xJP3XuybIE":
+        Script49();
+        break;
+      case "6kgZCEyosYk":
+        Script50();
+        break;
+      case "6dJIpuw1FmR":
+        Script51();
+        break;
+      case "5ynrnVdzAkC":
+        Script52();
+        break;
+      case "5tRTn8M5BK2":
+        Script53();
+        break;
+      case "5yvrFsBFqz5":
+        Script54();
+        break;
+      case "6ZnLjhCwkNz":
+        Script55();
+        break;
+      case "6CyBmbzCrrS":
+        Script56();
+        break;
+      case "5zLgWhs3bBO":
+        Script57();
+        break;
+      case "6a2KL8IVN9s":
+        Script58();
+        break;
+      case "6nLJWXftkFo":
+        Script59();
+        break;
+  }
+}
+
+function Script1()
+{
+  player = GetPlayer();
+var Sl_1_Answ_1=0;
+player.SetVar("1_Sl_Answer_1",Sl_1_Answ_1);
+}
+
+function Script2()
+{
+  player = GetPlayer();
+var Sl_1_Answ_2=0;
+player.SetVar("1_Sl_Answer_2",Sl_1_Answ_2);
+}
+
+function Script3()
+{
+  player = GetPlayer();
+var Sl_1_Answ_3=0;
+player.SetVar("1_Sl_Answer_3",Sl_1_Answ_3);
+}
+
+function Script4()
+{
+  player = GetPlayer();
+var Sl_1_Answ_1=3;
+player.SetVar("1_Sl_Answer_1",Sl_1_Answ_1);
+}
+
+function Script5()
+{
+  player = GetPlayer();
+var Sl_1_Answ_2=2;
+player.SetVar("1_Sl_Answer_2",Sl_1_Answ_2);
+}
+
+function Script6()
+{
+  player = GetPlayer();
+var Sl_1_Answ_3=1;
+player.SetVar("1_Sl_Answer_3",Sl_1_Answ_3);
+}
+
+function Script7()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("1_Sl_Answer_1")+player.GetVar("1_Sl_Answer_2")+player.GetVar("1_Sl_Answer_3");
+player.SetVar("ResultScore_1",ResScore_1);
+}
+
+function Script8()
+{
+  player = GetPlayer();
+var Sl_1_Answ_4=0;
+player.SetVar("1_Sl_Answer_4",Sl_1_Answ_4);
+}
+
+function Script9()
+{
+  player = GetPlayer();
+var Sl_2_Answ_1=0;
+player.SetVar("2_Sl_Answer_1",Sl_2_Answ_1);
+}
+
+function Script10()
+{
+  player = GetPlayer();
+var Sl_2_Answ_2=0;
+player.SetVar("2_Sl_Answer_2",Sl_2_Answ_2);
+}
+
+function Script11()
+{
+  player = GetPlayer();
+var Sl_2_Answ_3=0;
+player.SetVar("2_Sl_Answer_3",Sl_2_Answ_3);
+}
+
+function Script12()
+{
+  player = GetPlayer();
+var Sl_2_Answ_1=3;
+player.SetVar("2_Sl_Answer_1",Sl_2_Answ_1);
+}
+
+function Script13()
+{
+  player = GetPlayer();
+var Sl_2_Answ_2=2;
+player.SetVar("2_Sl_Answer_2",Sl_2_Answ_2);
+}
+
+function Script14()
+{
+  player = GetPlayer();
+var Sl_2_Answ_3=1;
+player.SetVar("2_Sl_Answer_3",Sl_2_Answ_3);
+}
+
+function Script15()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("2_Sl_Answer_1")+player.GetVar("2_Sl_Answer_2")+player.GetVar("2_Sl_Answer_3");
+player.SetVar("ResultScore_2",ResScore_1);
+}
+
+function Script16()
+{
+  player = GetPlayer();
+var Sl_2_Answ_4=0;
+player.SetVar("2_Sl_Answer_4",Sl_2_Answ_4);
+}
+
+function Script17()
+{
+  player = GetPlayer();
+var Sl_3_Answ_1=2;
+player.SetVar("3_Sl_Answer_1",Sl_3_Answ_1);
+}
+
+function Script18()
+{
+  player = GetPlayer();
+var Sl_3_Answ_1=0;
+player.SetVar("3_Sl_Answer_1",Sl_3_Answ_1);
+}
+
+function Script19()
+{
+  player = GetPlayer();
+var Sl_3_Answ_2=2;
+player.SetVar("3_Sl_Answer_2",Sl_3_Answ_2);
+}
+
+function Script20()
+{
+  player = GetPlayer();
+var Sl_3_Answ_2=0;
+player.SetVar("3_Sl_Answer_2",Sl_3_Answ_2);
+}
+
+function Script21()
+{
+  player = GetPlayer();
+var Sl_3_Answ_3=2;
+player.SetVar("3_Sl_Answer_3",Sl_3_Answ_3);
+}
+
+function Script22()
+{
+  player = GetPlayer();
+var Sl_3_Answ_3=0;
+player.SetVar("3_Sl_Answer_3",Sl_3_Answ_3);
+}
+
+function Script23()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("3_Sl_Answer_1")+player.GetVar("3_Sl_Answer_2")+player.GetVar("3_Sl_Answer_3");
+player.SetVar("ResultScore_3",ResScore_1);
+}
+
+function Script24()
+{
+  player = GetPlayer();
+var Sl_4_Answ_1=2;
+player.SetVar("4_Sl_Answer_1",Sl_4_Answ_1);
+}
+
+function Script25()
+{
+  player = GetPlayer();
+var Sl_4_Answ_1=0;
+player.SetVar("4_Sl_Answer_1",Sl_4_Answ_1);
+}
+
+function Script26()
+{
+  player = GetPlayer();
+var Sl_4_Answ_2=2;
+player.SetVar("4_Sl_Answer_2",Sl_4_Answ_2);
+}
+
+function Script27()
+{
+  player = GetPlayer();
+var Sl_4_Answ_2=0;
+player.SetVar("4_Sl_Answer_2",Sl_4_Answ_2);
+}
+
+function Script28()
+{
+  player = GetPlayer();
+var Sl_4_Answ_3=2;
+player.SetVar("4_Sl_Answer_3",Sl_4_Answ_3);
+}
+
+function Script29()
+{
+  player = GetPlayer();
+var Sl_4_Answ_3=0;
+player.SetVar("4_Sl_Answer_3",Sl_4_Answ_3);
+}
+
+function Script30()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("4_Sl_Answer_1")+player.GetVar("4_Sl_Answer_2")+player.GetVar("4_Sl_Answer_3");
+player.SetVar("ResultScore_4",ResScore_1);
+}
+
+function Script31()
+{
+  player = GetPlayer();
+var Sl_5_Answ_1=3;
+player.SetVar("5_Sl_Answer_1",Sl_5_Answ_1);
+}
+
+function Script32()
+{
+  player = GetPlayer();
+var Sl_5_Answ_1=0;
+player.SetVar("5_Sl_Answer_1",Sl_5_Answ_1);
+}
+
+function Script33()
+{
+  player = GetPlayer();
+var Sl_5_Answ_2=3;
+player.SetVar("5_Sl_Answer_2",Sl_5_Answ_2);
+}
+
+function Script34()
+{
+  player = GetPlayer();
+var Sl_5_Answ_2=0;
+player.SetVar("5_Sl_Answer_2",Sl_5_Answ_2);
+}
+
+function Script35()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("5_Sl_Answer_1")+player.GetVar("5_Sl_Answer_2");
+player.SetVar("ResultScore_5",ResScore_1);
+}
+
+function Script36()
+{
+  player = GetPlayer();
+var Sl_9_Answ_1=1;
+player.SetVar("9_Sl_Answer_1",Sl_9_Answ_1);
+}
+
+function Script37()
+{
+  player = GetPlayer();
+var Sl_9_Answ_1=0;
+player.SetVar("9_Sl_Answer_1",Sl_9_Answ_1);
+}
+
+function Script38()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("9_Sl_Answer_1")+player.GetVar("9_Sl_Answer_2")+player.GetVar("9_Sl_Answer_3")+player.GetVar("9_Sl_Answer_4")+player.GetVar("9_Sl_Answer_5");
+player.SetVar("ResultScore_9",ResScore_1);
+}
+
+function Script39()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("ResultScore_1")+player.GetVar("ResultScore_2")+player.GetVar("ResultScore_3")+player.GetVar("ResultScore_4")+player.GetVar("ResultScore_5")+player.GetVar("ResultScore_6")+player.GetVar("ResultScore_7")+player.GetVar("ResultScore_8")+player.GetVar("ResultScore_9");
+player.SetVar("ResultScore",ResScore_1);
+}
+
+function Script40()
+{
+  player=GetPlayer();
+
+var Temp=player.GetVar("Temp_Vlv");
+var Combi=player.GetVar("Combi_Vlv");
+var Bk=player.GetVar("BK_Vlv");
+var PassScore=16;
+
+if (Temp == true && Combi == true && Bk== true)
+    { PassScore=23;
+      console.log(Temp);
+ } else if (Temp == true && Combi == true && Bk == false)
+    { PassScore=22;
+ } else if ((Temp == false && Combi == true && Bk == true) || (Temp == true && Combi == false && Bk == true))
+    { PassScore=20;
+ } else if ((Temp == false && Combi == true && Bk == false) || (Temp == true && Combi == false && Bk == false))
+    { PassScore=19;
+ } else if (Temp == false && Combi == false && Bk == true)
+    { PassScore=18;
+ } else if (Temp == false && Combi == false && Bk == false)
+    { PassScore=16;
+ } else {}
+
+player.SetVar("Pass_Score",PassScore);
+}
+
+function Script41()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("Add_Balls");
+player.SetVar("ResultScore_10",ResScore_1);
+}
+
+function Script42()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("ResultScore_1")+player.GetVar("ResultScore_2")+player.GetVar("ResultScore_3")+player.GetVar("ResultScore_4")+player.GetVar("ResultScore_5")+player.GetVar("ResultScore_6")+player.GetVar("ResultScore_7")+player.GetVar("ResultScore_8")+player.GetVar("ResultScore_9")+player.GetVar("ResultScore_10");
+player.SetVar("ResultScore",ResScore_1);
+}
+
+function Script43()
+{
+  var player=GetPlayer();
+
+var js_user_name=player.GetVar("UserName");
+var js_user_last_name=player.GetVar("UserSurname")
+var total_score=player.GetVar("ResultScore");
+var usr_email=player.GetVar("UserEmail");
+var rtc_email=player.GetVar("RegTrEmail")
+
+var email=usr_email+";"+rtc_email;
+var subject=js_user_name + " "+ js_user_last_name;
+var body_start=" Добрый день! %0A %0A "+js_user_name+ " "+ js_user_last_name + " не справился(лась) с заданием \"Работа с проектной документацией. Индивидуальные тепловые пункты\" квалификационной работы для инженера по продажам.  %0A Итоговое количество баллов = "+total_score+". Пожалуйста, внестите эти данные в соотвествующий тест в системе CornerStone для завершения выполнения заданий квалификационной работы. %0A%0A  Cornerstone";
+
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script44()
+{
+  var player=GetPlayer();
+
+var js_user_name=player.GetVar("UserName");
+var js_user_last_name=player.GetVar("UserSurname")
+var total_score=player.GetVar("ResultScore");
+var usr_email=player.GetVar("UserEmail");
+var rtc_email=player.GetVar("RegTrEmail")
+
+var email=usr_email+";"+rtc_email;
+var subject=js_user_name + " "+ js_user_last_name;
+var body_start=" добрый день! %0A %0A "+js_user_name+ " "+ js_user_last_name + " успешно справился(лась) с заданием \"Работа с проектной документацией. Индивидуальные тепловые пункты\" квалификационной работы для инженера по продажам %0A Итоговое количество баллов = "+total_score+". Пожалуйста, внестите эти данные в соотвествующий тест в системе CornerStone для продолжения выполнения заданий квалификационной работы. %0A%0A  Cornerstone";
+
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script45()
+{
+  player = GetPlayer();
+var Sl_6_Answ_1=2;
+player.SetVar("6_Sl_Answer_1",Sl_6_Answ_1);
+}
+
+function Script46()
+{
+  player = GetPlayer();
+var Sl_6_Answ_1=0;
+player.SetVar("6_Sl_Answer_1",Sl_6_Answ_1);
+}
+
+function Script47()
+{
+  player = GetPlayer();
+var Sl_6_Answ_2=2;
+player.SetVar("6_Sl_Answer_2",Sl_6_Answ_2);
+}
+
+function Script48()
+{
+  player = GetPlayer();
+var Sl_6_Answ_2=0;
+player.SetVar("6_Sl_Answer_2",Sl_6_Answ_2);
+}
+
+function Script49()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("6_Sl_Answer_1")+player.GetVar("6_Sl_Answer_2");
+player.SetVar("ResultScore_6",ResScore_1);
+}
+
+function Script50()
+{
+  player = GetPlayer();
+var Sl_7_Answ_1=2;
+player.SetVar("7_Sl_Answer_1",Sl_7_Answ_1);
+}
+
+function Script51()
+{
+  player = GetPlayer();
+var Sl_7_Answ_1=0;
+player.SetVar("7_Sl_Answer_1",Sl_7_Answ_1);
+}
+
+function Script52()
+{
+  player = GetPlayer();
+var Sl_7_Answ_2=2;
+player.SetVar("7_Sl_Answer_2",Sl_7_Answ_2);
+}
+
+function Script53()
+{
+  player = GetPlayer();
+var Sl_7_Answ_2=0;
+player.SetVar("7_Sl_Answer_2",Sl_7_Answ_2);
+}
+
+function Script54()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("7_Sl_Answer_1")+player.GetVar("7_Sl_Answer_2");
+player.SetVar("ResultScore_7",ResScore_1);
+}
+
+function Script55()
+{
+  player = GetPlayer();
+var Sl_8_Answ_1=2;
+player.SetVar("8_Sl_Answer_1",Sl_8_Answ_1);
+}
+
+function Script56()
+{
+  player = GetPlayer();
+var Sl_8_Answ_1=0;
+player.SetVar("8_Sl_Answer_1",Sl_8_Answ_1);
+}
+
+function Script57()
+{
+  player = GetPlayer();
+var Sl_8_Answ_2=2;
+player.SetVar("8_Sl_Answer_2",Sl_8_Answ_2);
+}
+
+function Script58()
+{
+  player = GetPlayer();
+var Sl_8_Answ_2=0;
+player.SetVar("8_Sl_Answer_2",Sl_8_Answ_2);
+}
+
+function Script59()
+{
+  player = GetPlayer();
+ResScore_1=player.GetVar("8_Sl_Answer_1")+player.GetVar("8_Sl_Answer_2");
+player.SetVar("ResultScore_8",ResScore_1);
+}
+
